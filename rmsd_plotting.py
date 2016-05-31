986105db167964aff370872b72872ce1f4e47e14
rbdavid <russell.davidson@colostate.edu>
2016-05-31 11:30:44 -0600

    edited the gitignore file; added the basic rmsd script calculating the rmsd of atom selections defined in the sel_list.py script

---
+++ rmsd_plotting.py
@@ -0,0 +1,34 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+# USAGE:
+
+# PREAMBLE:
+
+from plotting_functions import *
+import sel_list
+
+dat = sys.argv[1]  
+system = sys.argv[2]
+
+nSel = len(sel_list.sel)
+
+equilib = 0
+
+# ----------------------------------------
+# MAIN PROGRAM:
+
+datalist = np.loadtxt(dat)
+
+nSteps = len(datalist[:,0])
+print 'Number of selections: %d, number of steps: %d' %(nSel,nSteps)
+
+time = np.zeros(nSteps)
+
+for i in range(nSteps):
+	time[i] = i*0.002		# units of time in ns; each frame is separated by 0.002 ns 
+
+for i in range(nSel):
+	selection = sel_list.sel[i][0]
+
+	scat_hist(time[:],datalist[:,i],'k','Time (ns)','RMSD','%02d.%s' %(i,selection),'%s' %(system),yunits='$\AA$')
+
+
