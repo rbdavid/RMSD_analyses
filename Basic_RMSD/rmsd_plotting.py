509a5c58cc1eb296c1521b6cbad9dc5cb94249e6
Russell Bruce Davidson <Russell.Davidson@colostate.edu>
2016-10-18 21:32:26 -0600

    updated the plotting script; read in the selection_list.out file (with three spaces as delimiter) and plots will be named appropriately (kind of); wouldn't mind cleaning up the names being used...

--- Basic_RMSD/rmsd_plotting.py
+++ Basic_RMSD/rmsd_plotting.py
@@ -4,29 +4,29 @@
 # PREAMBLE:
 
 from plotting_functions import *
-from sel_list import *
 
-dat = sys.argv[1]  
-system = sys.argv[2]
-
-nSel = len(sel)
+data_file = sys.argv[1]  
+selection_list = sys.argv[2]
+system = sys.argv[3]
 
 # ----------------------------------------
 # MAIN:
 
-datalist = np.loadtxt(dat)
-
-nSteps = len(datalist[:,0])
-print 'Number of selections: %d, number of steps: %d' %(nSel,nSteps)
+datalist = np.loadtxt(data_file)
+nSels = len(datalist[0])
+nSteps = len(datalist)
+print 'Number of selections: %d, number of steps: %d' %(nSels,nSteps)
 
 time = np.zeros(nSteps)
-
 for i in range(nSteps):
 	time[i] = i*0.002		# units of time in ns; each frame is separated by 0.002 ns 
 
+selection_titles = []
+with open(selection_list,'r') as f:
+	for line in f:
+		temp = line.split('   ')
+		selection_titles.append('%02d.%s'%(int(temp[0]),temp[1]))
+
 for i in range(nSel):
-	selection = sel[i][0]
-
-	scat_hist(time[:],datalist[:,i],'k','Time (ns)','RMSD','%02d.%s' %(i,selection),'%s' %(system),yunits='$\AA$')
-
+	scat_hist(time[:],datalist[:,i],'k','Time (ns)','RMSD',selection_titles[i],'%s' %(system),yunits='$\AA$')
 
